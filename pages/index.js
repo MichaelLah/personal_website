60fa01af9799d7442ea20d3e739e0a44215cd1bd
MichaelLah <michael@michaellah.com>
2021-01-19 01:49:05 -0500

    nextjs prep

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>Michael Lah</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Michael Lah</h1>
+      </main>
+    </>
+  );
+};
+export default Home;
