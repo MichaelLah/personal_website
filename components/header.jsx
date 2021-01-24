c37e4f7f9de6e28cd2f46599e16c409b26e3ac4b
MichaelLah <michael@michaellah.com>
2021-01-24 18:47:32 -0500

    Add header

--- components/header.jsx
+++ components/header.jsx
@@ -1,13 +1,17 @@
 import Head from "next/head";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <>
-      <div id="header_name">
-          Lah
+      <div id="header_name">Lah</div>
+      <div id="links">
+        <Link href="Lah_spring_2019_resume.pdf">Resume</Link>
+        <Link href="https://github.com/MichaelLah">GitHub</Link>
+        <Link href="/photography">Photography</Link>
       </div>
     </>
   );
-}
+};
 
-export default Header
+export default Header;
