5b433e71a4c0de691c8a6cb83c9f04b4cba62134
MichaelLah <michael@michaellah.com>
2021-01-24 19:25:51 -0500

    add more to header

--- components/header.jsx
+++ components/header.jsx
@@ -1,16 +1,33 @@
 import Head from "next/head";
+import styled from "styled-components";
 import Link from "next/link";
+
+const StyledHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 15px;
+  #header_name {
+      margin: 16px;
+  }
+`;
+
+const StyledLinks = styled.ul`
+  a {
+    padding: 5px;
+  }
+`;
 
 const Header = () => {
   return (
-    <>
+    <StyledHeader>
       <div id="header_name">Lah</div>
-      <div id="links">
+      <StyledLinks>
         <Link href="Lah_spring_2019_resume.pdf">Resume</Link>
         <Link href="https://github.com/MichaelLah">GitHub</Link>
         <Link href="/photography">Photography</Link>
-      </div>
-    </>
+        <Link href="/contact">Contact</Link>
+      </StyledLinks>
+    </StyledHeader>
   );
 };
 
